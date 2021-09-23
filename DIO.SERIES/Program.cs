54df0d1193bfb07087fb77f7f8e102d45fc4afe8
Vitoria Akemi <rm88077@fiap.com.br>
2021-09-23 14:58:05 -0300

    criação do projeto .net

---
+++ DIO.SERIES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.SERIES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
