f0a3bc0fbc601724faa09adcc4c25cfbd9c8c083
Vitoria Akemi <rm88077@fiap.com.br>
2021-09-30 14:54:04 -0300

    criação da de arquivo dotnet

---
+++ ApiCatalogoJogos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiCatalogoJogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
